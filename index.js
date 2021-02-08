1664684f7b65b290aa6c84a650c47ff97fa99512
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2021-02-09 01:05:38 +0600

    image change

--- index.js
+++ index.js
@@ -6,7 +6,17 @@
 // function para(){
 //   alert("Hi i am a paragrap");
 // }
-function para(){
-var myVar =  document.querySelector("#paraid");
-  myVar.innerHTML ="You have click the buton";
+//querySelector
+
+// function para(){
+// var myVar =  document.querySelector("#paraid");
+//   myVar.innerHTML ="You have click the button";
+// }
+function pic1(){
+  var myVariable = document.querySelector("#myImage");
+  myVariable.src = "img/image1.jpeg";
+}
+function pic2(){
+  var myVariable = document.querySelector("#myImage");
+  myVariable.src = "img/image2.jpeg";
 }
