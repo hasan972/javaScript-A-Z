e3446176e768ec572220bac16741a00d8b661084
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2021-02-09 12:11:36 +0600

    Create Html tag

--- index.js
+++ index.js
@@ -12,11 +12,21 @@
 // var myVar =  document.querySelector("#paraid");
 //   myVar.innerHTML ="You have click the button";
 // }
-function pic1(){
-  var myVariable = document.querySelector("#myImage");
-  myVariable.src = "img/image1.jpeg";
-}
-function pic2(){
-  var myVariable = document.querySelector("#myImage");
-  myVariable.src = "img/image2.jpeg";
-}
+//Image change functioin 313
+// function pic1(){
+//   var myVariable = document.querySelector("#myImage");
+//   myVariable.src = "img/image1.jpeg";
+// }
+// function pic2(){
+//   var myVariable = document.querySelector("#myImage");
+//   myVariable.src = "img/image2.jpeg";
+// }
+
+
+//create a html tag using javascript
+var heading3 = document.createElement("h2");//create element
+var text = document.createTextNode("This is heading3");
+heading3.appendChild(text);
+//ad the text to html code 
+var myDiv = document.getElementById("my");
+myDiv.appendChild(heading3);
