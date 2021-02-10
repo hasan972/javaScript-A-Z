978ecf400a64ccef86b3c45e7177fd9a291f6781
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2021-02-11 02:03:51 +0600

    Key press Listener

--- index.js
+++ index.js
@@ -87,12 +87,19 @@ for( var i = 0; i < 3; i++){
   document.querySelectorAll(".myButton")[i].addEventListener("click",function (){
     var text = this.innerHTML;
     audioPlay(text);
+    playAnimation(text);
   });
 }
+
+document.addEventListener("keypress", function(event){
+  var text = event.key;
+  audioPlay(text);
+
+});
 //work the element
 function audioPlay(text){
 switch (text) {
-    case "First":
+    case "a":
       var audio = new Audio("sounds/a.mp3");
       audio.play();
       break;
@@ -107,4 +114,10 @@ switch (text) {
   default:
 
  }
+}
+//keypressListener
+
+function playAnimation(text){
+  var selectedButton = document.querySelector("."+text);
+  selectedButton.classList.add("anim");
 }
