9e609c6c10234acaedcace7a0aa007d889a53b8e
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2021-02-15 23:59:44 +0600

    Array destruction 1

--- 56-60.js
+++ 56-60.js
@@ -1,7 +1,10 @@
 //array destruction
-let number = [10,20,30]
-console.log(number[0])
-console.log(number[1])
+let number = [10,20,30,40,50,60,70]
+let [num1,num2,num3,num4] = number
+console.log(num1)
+console.log(num4)
+
+
 //swap variable
 //objective destruction
 //nasted objected destruction
